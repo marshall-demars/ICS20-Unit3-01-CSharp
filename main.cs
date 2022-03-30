6ac1d5d38c42fda69c3a495745e9b61b8c120c2e
marshall-demars <99354910+marshall-demars@users.noreply.github.com>
2022-03-30 17:22:38 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,42 @@
+// Created by: Marshall Demars
+// Created on: Mar 2022
+//
+// This program accepts user input
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function accepts user input
+        double height;
+        double baseA;
+        double baseB;
+        double area;
+
+        // input
+        Console.WriteLine("This program finds the area of a trapezoid.");
+        Console.WriteLine("");
+        Console.WriteLine("the formula is: [(a + b) / 2] * h");
+        Console.WriteLine("");
+        Console.WriteLine("please enter the measurements (m)");
+        Console.WriteLine("");
+
+        Console.Write("Input the measurement for base a: ");
+        baseA = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Input the measurement for base b: ");
+        baseB = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Input the measurement for the height: ");
+        height = Convert.ToDouble(Console.ReadLine());
+
+        // process
+        area = ((baseA + baseB) / 2) * height;
+
+        // output
+        Console.WriteLine("");
+        Console.WriteLine("The area of the trapezoid is, " + area.ToString("0.00") + " m²");
+        
+        Console.WriteLine("\nDone.");
+    }
+}
